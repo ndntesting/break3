a687f0bc628b7434cb3b1ce6cfbbb69c2ccaf360
ndntesting <ndntesting@outlook.com>
2018-06-12 18:56:33 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/break3/*.csproj && dotnet build src/break3/*.csproj
---
+++ src/break3/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace break3
+{
+    public class Class1
+    {
+    }
+}
